951c853bc2350aa6a296783911474ccab2849705
Nilesh Sawant <nsawant@kl6.head.cm.kestrel.hpc.nrel.gov>
2026-02-13 12:08:29 -0700

    Fix MPI+CUDA build for Cray systems with GPU-aware MPI support
    - Updated make.sh to use nvcc with Cray MPICH paths and GTL library
    - Added MPICH_GPU_SUPPORT_ENABLED flag to run.sh for GPU-aware MPI
    - Target H100 GPU with compute capability 9.0 (sm_90)

--- src/mpi_host_device/make.sh
+++ src/mpi_host_device/make.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-MPI_INCLUDE=$(mpic++ --showme:compile)
-MPI_LINK=$(mpic++ --showme:link)
-CUDA_MPI_LINK="-L/usr/local/cuda-12.6/lib64 -lcudart"
-nvcc -arch=sm_61 -x cu -std=c++17 --expt-extended-lambda \
-     $MPI_INCLUDE $MPI_LINK $CUDA_MPI_LINK mpiHostDevice.cpp -o mpiHostDevice.exe
+module load PrgEnv-gnu/8.5.0
+module load craype-x86-milan
+module load cuda/12.3
+
+# On Cray systems with PrgEnv-gnu, get MPI paths from environment
+# Use nvcc directly for CUDA compilation
+MPI_INCLUDE="-I${CRAY_MPICH_DIR}/include"
+MPI_LINK="-L${CRAY_MPICH_DIR}/lib -lmpich"
+CUDA_LINK="-L${CUDA_HOME}/lib64 -lcudart"
+# Try to find and link GTL for GPU-aware MPI support
+GTL_LINK="-L${CRAY_MPICH_ROOTDIR}/gtl/lib -lmpi_gtl_cuda"
+
+nvcc -arch=sm_90 -x cu -std=c++17 --expt-extended-lambda \
+     $MPI_INCLUDE $MPI_LINK $CUDA_LINK $GTL_LINK mpiHostDevice.cpp -o mpiHostDevice.exe
 
--- src/mpi_host_device/run.sh
+++ src/mpi_host_device/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Enable GPU-aware MPI support for Cray MPICH
+export MPICH_GPU_SUPPORT_ENABLED=1
+
 # Use the mpi built with cuda support
-/usr/openmpi-cuda/bin/mpirun -np 2 ./mpiHostDevice.exe
+srun -n 2 ./mpiHostDevice.exe
